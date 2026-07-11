aa7573da54471414d8157914710983092befa023
Kurtt.z <91841336+xKurty06@users.noreply.github.com>
2026-07-12 00:45:59 +0800

    feat: mount row preview controls

--- frontend/src/components/layout/AppShell.tsx
+++ frontend/src/components/layout/AppShell.tsx
@@ -14,6 +14,7 @@ import { useEffect, useState, type CSSProperties } from "react";
 import { NavLink, Outlet, useLocation } from "react-router-dom";
 import { appConfig } from "../../app/config";
 import { CommonFieldsChooser } from "../validation/CommonFieldsChooser";
+import { RowPreviewControls } from "../validation/RowPreviewControls";
 
 interface NavigationSection {
   label: string;
@@ -191,6 +192,7 @@ export function AppShell() {
           </div>
         </div>
       </main>
+      <RowPreviewControls />
       <CommonFieldsChooser />
     </div>
   );
