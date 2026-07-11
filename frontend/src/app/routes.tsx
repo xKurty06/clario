e3d991d16863a5b0b0ed4632c2f6b3e0bec2cc54
Kurtt.z <91841336+xKurty06@users.noreply.github.com>
2026-07-11 08:25:06 +0800

    fix: route builder through preset chooser

--- frontend/src/app/routes.tsx
+++ frontend/src/app/routes.tsx
@@ -1,6 +1,6 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { AppShell } from "../components/layout/AppShell";
-import { ComparisonBuilderPage } from "../pages/ComparisonBuilderPage";
+import { PresetAwareComparisonBuilderPage } from "../pages/PresetAwareComparisonBuilderPage";
 import { DashboardPage } from "../pages/DashboardPage";
 import { ReportExportPage } from "../pages/ReportExportPage";
 import { TemplateManagerPage } from "../pages/TemplateManagerPage";
@@ -13,7 +13,7 @@ export function AppRoutes() {
       <Route element={<AppShell />}>
         <Route index element={<DashboardPage />} />
         <Route path="upload" element={<UploadFilesPage />} />
-        <Route path="mapping" element={<ComparisonBuilderPage />} />
+        <Route path="mapping" element={<PresetAwareComparisonBuilderPage />} />
         <Route path="preview" element={<Navigate to="/mapping" replace />} />
         <Route path="results" element={<ValidationResultsPage />} />
         <Route path="reports" element={<ReportExportPage />} />
